bc20a0254badeb3cd675479fd1d377e37a0988d7
Fiona Wilson <fiona.wilson@resdiary.com>
2023-12-01 13:52:15 +0000

    Initial Commit

---
+++ WordleClone/WordleCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleCloneApp.swift
+//  WordleClone
+//
+//  Created by Fiona Wilson on 01/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordleCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
